eb1b19e335dfb4a0f33f7fad8984b6c9983b6a96
gruuubyy <wiktordzidziul@gmail.com>
2025-01-28 10:21:21 +0100

    Working project

--- control_room.c
+++ control_room.c
@@ -10,12 +10,14 @@
 #include <sys/sem.h>
 #include <sys/msg.h>
 #include <sys/types.h>
+#include <sys/sem.h>
 
 #define MAGAZINES 3
 #define COURIERS 3
 #define PRODUCT 3
 #define SHM_SIZE 1024
-
+#define SEM_KEY 6666
+#define MEMORY_FREE 0
 
 // to jako argumenty
 #define KEY 5555
@@ -40,6 +42,7 @@ struct control_room {
 struct shared_data {
     struct magazine magazines[MAGAZINES];
     struct control_room ctrl_room;
+    int sem_ready;
 };
 
 struct order_info {
@@ -65,7 +68,18 @@ int main(int argc, char* argv[]) {
 
     int shmid = shmget(KEY, SHM_SIZE, 0666 | IPC_CREAT);
     struct shared_data* data = (struct shared_data*)shmat(shmid, NULL, 0);
+    int semid = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
+    semctl(semid, MEMORY_FREE, SETVAL, 1);
+    data->sem_ready = 1;
+    
+    struct sembuf memory_lock = {MEMORY_FREE, -1, 0};
+    struct sembuf memory_unlock = {MEMORY_FREE, 1, 0};
+
+    semop(semid, &memory_lock, 1);
+
     data->ctrl_room.control_room_active = 1;
+
+    semop(semid, &memory_unlock, 1);
 
     int sleeping_time;
     int min_sleep = 25000; // 0.025s
@@ -76,9 +90,9 @@ int main(int argc, char* argv[]) {
         int sleeping_time = rand() % (max_sleep - min_sleep + 1) + min_sleep;
         usleep(sleeping_time);
 
-        a = (rand() % (MAX_A - 1)) + 1;
-        b = (rand() % (MAX_B - 1)) + 1;
-        c = (rand() % (MAX_C - 1)) + 1;
+        a = (rand() % (MAX_A + 1));
+        b = (rand() % (MAX_B + 1));
+        c = (rand() % (MAX_C + 1));
 
         message.order.product[0] = a;
         message.order.product[1] = b;
@@ -91,12 +105,15 @@ int main(int argc, char* argv[]) {
 
     int mag_active = 0;
     while (data->ctrl_room.control_room_active) {
-        sleep(1);
+        mag_active = 0;
+        sleep(2);
         for (int i = 0; i < MAGAZINES; i ++) {
             mag_active += data->magazines[i].magazine_active;
         }
         if (mag_active == 0) {
+            semop(semid, &memory_lock, 1);
             data->ctrl_room.control_room_active = 0;
+            semop(semid, &memory_unlock, 1);
         }
     }
 
@@ -111,6 +128,8 @@ int main(int argc, char* argv[]) {
     printf("Memory detached\n");
     shmctl(shmid, IPC_RMID, NULL);
     printf("Memory marked to delete\n");
+    semctl(semid, 0, IPC_RMID);
+    printf("Semafor usunięty\n");
 
     return 0;
 }
--- magazine1.c
+++ magazine1.c
@@ -12,11 +12,16 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <time.h>
+#include <sys/sem.h>
 
 #define COURIERS 3
 #define PRODUCT 3
 #define MAGAZINES 3
 #define SHM_SIZE 1024
+#define WAITING_TIME 15
+#define SEM_KEY 6666
+#define MEMORY_FREE 0
 
 #define MAGAZINE_NUMBER 1
 
@@ -39,6 +44,7 @@ struct control_room {
 struct shared_data {
     struct magazine magazines[MAGAZINES];
     struct control_room ctrl_room;
+    int sem_ready;
 };
 
 struct order_info {
@@ -61,8 +67,18 @@ int main(int argc, char* argv[]) {
     int shmid = shmget(KEY, SHM_SIZE, 0666 | IPC_CREAT);
     struct shared_data* data = (struct shared_data*)shmat(shmid, NULL, 0);
 
+    int semid = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
+    struct sembuf memory_lock = {MEMORY_FREE, -1, 0};
+    struct sembuf memory_unlock = {MEMORY_FREE, 1, 0};
+
+    while (!data->sem_ready) {
+        usleep(100000);
+    }
+
     int pids[COURIERS];
     int current_courier = -1;
+
+    semop(semid, &memory_lock, 1);
 
     data->magazines[MAGAZINE_NUMBER].magazine_active = 1;
     data->magazines[MAGAZINE_NUMBER].magazine_gold = 0;
@@ -71,12 +87,13 @@ int main(int argc, char* argv[]) {
         data->magazines[MAGAZINE_NUMBER].courier_active[i] = 1;
     }
 
+    semop(semid, &memory_unlock, 1);
+
     int fd = open("mag1.txt", O_RDONLY);
     if (fd == -1) {
         perror("open");
         exit(1);
     }
-
     char buf[1];
     int bytes_read;
     int tmp[2 * PRODUCT] = {0};
@@ -102,12 +119,16 @@ int main(int argc, char* argv[]) {
     }
     close(fd);
 
+    semop(semid, &memory_lock, 1);
+
     for (int i = 0; i < PRODUCT; i++) {
         data->magazines[MAGAZINE_NUMBER].product_number[i] = tmp[i];
         data->magazines[MAGAZINE_NUMBER].product_cost[i] = tmp[i + PRODUCT];
         // printf("%d\n", data->magazines[MAGAZINE_NUMBER].product_number[i]);
         // printf("%d\n", data->magazines[MAGAZINE_NUMBER].product_cost[i]);
     }
+
+    semop(semid, &memory_unlock, 1);
 
     for (int i = 0; i < COURIERS; i++) {
         pids[i] = fork();
@@ -118,21 +139,33 @@ int main(int argc, char* argv[]) {
             int min_sleep = 100000;
             int max_sleep = 500000;
             int gold = 0;
+            int last_order_time = time(NULL);
+            int current_time;
             while (data->magazines[MAGAZINE_NUMBER].courier_active[i]) {
                 sleeping_time = rand() % (max_sleep - min_sleep + 1) + min_sleep;
                 usleep(sleeping_time);
 
+                current_time = time(NULL);
+                if (difftime(current_time, last_order_time) > WAITING_TIME) {
+                    data->magazines[MAGAZINE_NUMBER].courier_active[i] = 0;
+                    printf("Kurier %d magazynu %d wyłącza się po %d sekundach bez zlecenia\n", i, MAGAZINE_NUMBER, WAITING_TIME);
+                    exit(0);
+                }
+
+
                 if (msgrcv(msgid, &message, sizeof(struct order_info), 1, IPC_NOWAIT) > 0) {
-                    // semafor
+                    last_order_time = time(NULL);
                     if (message.order.product[0] > data->magazines[MAGAZINE_NUMBER].product_number[0] ||
                         message.order.product[1] > data->magazines[MAGAZINE_NUMBER].product_number[1] ||
                         message.order.product[2] > data->magazines[MAGAZINE_NUMBER].product_number[2]) {
                             data->magazines[MAGAZINE_NUMBER].courier_active[i] = 0;
-                            printf("Kurier %d, magazynu %d, wylacza sie\n", i, MAGAZINE_NUMBER);
+                            printf("Kurier %d, magazynu %d, wylacza sie z powodu braku zasobow(zamowienie %d)\n", i, MAGAZINE_NUMBER, message.order.order_number);
                     } else {
                         printf("Kurier %d, magazynu %d, przyjął zamówienie numer %d, na %dxA, %dxB, %dxC\n", 
                         i, MAGAZINE_NUMBER, message.order.order_number, message.order.product[0],
                         message.order.product[1], message.order.product[2]);
+
+                        semop(semid, &memory_lock, 1);
 
                         for (int j = 0; j < PRODUCT; j++) {
                             data->magazines[MAGAZINE_NUMBER].product_number[j] -= message.order.product[j];
@@ -140,6 +173,7 @@ int main(int argc, char* argv[]) {
                             data->magazines[MAGAZINE_NUMBER].magazine_gold += gold;
                             data->ctrl_room.control_room_gold += gold;
                         }
+                        semop(semid, &memory_unlock, 1);
                     }
                 }
             }
@@ -150,6 +184,9 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < COURIERS; i++) {
         wait(NULL);
     }
+
+    semop(semid, &memory_lock, 1);
+
     data->magazines[MAGAZINE_NUMBER].magazine_active = 0;
 
     printf("\n======================================================\n");
@@ -159,6 +196,8 @@ int main(int argc, char* argv[]) {
     }
     printf("Gold: %d\n", data->magazines[MAGAZINE_NUMBER].magazine_gold);
     printf("======================================================\n");
+
+    semop(semid, &memory_unlock, 1);
 
     shmdt(data);
     printf("Memory detached\n");
